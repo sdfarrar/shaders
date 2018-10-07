bf38f888749df2ee38e691a94e08d95dc2a03cd9
vague315 <vague315@gmail.com>
2018-10-07 14:03:19 -0500

    Adds regions for smoke and gol areas to update render texture

--- Assets/Fluid/DrawMouse.cs
+++ Assets/Fluid/DrawMouse.cs
@@ -3,49 +3,59 @@ using UnityEngine;
 
 public class DrawMouse : MonoBehaviour {
 
-    public RenderTexture renderTexture;
+    public RenderTexture RenderTexture;
 
     public Texture brush0;
     public Texture brush1;
     public int size = 50;
 
     private Material _addBrushMaterial;
+    private RenderTexture initialTexture;
+
+    void Awake() { 
+        SetTextureRegion.OnRegionEntered += SetRenderTexture;
+    }
 
     void Start() {
         _addBrushMaterial = new Material(Shader.Find("Hidden/AddBrushShader"));
+        initialTexture = RenderTexture;
     }
 
-	// Update is called once per frame
-	void Update () {
+	void Update() {
+        if(!Input.GetMouseButton(0) && !Input.GetMouseButton(1)){ return; }
+        if(RenderTexture==null || brush0==null || brush1==null){ Debug.LogWarning("Null values present!"); return; }
+        DrawToTexture();
+    }
 
-        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
-        {
-            Texture brush = Input.GetMouseButton(0) ? brush0 : brush1;
+    private void DrawToTexture(){
+        Texture brush = Input.GetMouseButton(0) ? brush0 : brush1;
 
-            Vector3 mouse = Input.mousePosition;
-            mouse.z = transform.position.z;
-            Vector2 screen = -Camera.main.ScreenToWorldPoint(mouse);
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = transform.position.z;
 
-            Debug.DrawLine(Vector3.zero, screen);
+        //Vector2 screen = -Camera.main.ScreenToWorldPoint(mouse);
+        //Debug.DrawLine(Vector3.zero, screen);
 
-            Camera cam = Camera.main;
-            RaycastHit hit;
-            if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit)) return;
+        Camera cam = Camera.main;
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit)) return;
 
-            Renderer rend = hit.transform.GetComponent<Renderer>();
-            MeshCollider meshCollider = hit.collider as MeshCollider;
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
 
-            float x = hit.textureCoord.x * renderTexture.width - size*0.5f;
-            float y = (1-hit.textureCoord.y) * renderTexture.height - size*0.5f;
+        float x = hit.textureCoord.x * RenderTexture.width - size*0.5f;
+        float y = (1-hit.textureCoord.y) * RenderTexture.height - size*0.5f;
 
-            RenderTexture.active = renderTexture; //the render texture to be drawn to
-            GL.PushMatrix();
-            GL.LoadPixelMatrix(0, renderTexture.width, renderTexture.height, 0);
-            Graphics.DrawTexture(new Rect(x, y, size, size), brush);
-            GL.PopMatrix();
-            RenderTexture.active = null;
-        }
+        RenderTexture.active = RenderTexture; //the render texture to be drawn to
+        GL.PushMatrix();
+        GL.LoadPixelMatrix(0, RenderTexture.width, RenderTexture.height, 0);
+        Graphics.DrawTexture(new Rect(x, y, size, size), brush);
+        GL.PopMatrix();
+        RenderTexture.active = null;
+    }
 
+    private void SetRenderTexture(SetTextureRegion region) {
+        RenderTexture = region.TargetTexture;
     }
 
 }
---
+++ Assets/Showcase/Scripts/SetTextureRegion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SetTextureRegion : MonoBehaviour {
+
+	public static event Action<SetTextureRegion> OnRegionEntered = delegate { };
+
+	public RenderTexture TargetTexture;
+
+	private new BoxCollider collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<BoxCollider>();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(!other.gameObject.CompareTag("Player")){ return ; }
+
+		OnRegionEntered(this);
+		
+	}
+
+}
